8d3c4f7d55aeecbbfb322eca8373a75f2055f811
jmtid <jmtidone@gmail.com>
2021-01-14 16:16:06 -0300

    First App Commit

---
+++ src/main/java/com/Simplehelloworld/Simplehelloworld/SimplehelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.Simplehelloworld.Simplehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplehelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplehelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Simplehelloworld/Simplehelloworld/SimplehelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Simplehelloworld.Simplehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplehelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
